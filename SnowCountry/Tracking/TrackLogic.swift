8723a76c4710081e22ff3b61f6f3b31e1d91455f
rjpotter <rjpotter@uvm.edu>
2023-11-30 12:57:22 -0500

    New tracking display

--- SnowCountry/Tracking/TrackLogic.swift
+++ SnowCountry/Tracking/TrackLogic.swift
@@ -6,43 +6,90 @@
 //
 
 import CoreLocation
+import Foundation
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
     @Published var locations: [CLLocation] = []
-    private var lastLocation: CLLocation? // To track the last updated location
+    private var lastLocation: CLLocation?
+
+    // Additional tracking properties
+    @Published var totalDistance: Double = 0.0 // Total distance in meters
+    @Published var currentSpeed: Double = 0.0 // Speed in meters per second
+    @Published var maxSpeed: Double = 0.0 // Maximum speed in meters per second
+    @Published var currentAltitude: Double = 0.0 // Current altitude in meters
+    @Published var totalElevationGain: Double = 0.0 // Total elevation gain in meters
+    @Published var recordingDuration: TimeInterval = 0 // Duration in seconds
+    private var startTime: Date? // Start time of the recording
 
     override init() {
         super.init()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.distanceFilter = 3 // Update every 3 meters
+        locationManager.distanceFilter = 3
         locationManager.requestWhenInUseAuthorization()
+        
+        // For background location updates
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.pausesLocationUpdatesAutomatically = false
     }
 
     func startTracking() {
+        startTime = Date() // Set the start time
         locationManager.startUpdatingLocation()
     }
 
     func stopTracking() {
         locationManager.stopUpdatingLocation()
+        if let startTime = startTime {
+            recordingDuration = Date().timeIntervalSince(startTime)
+        }
         saveLocationsToFile()
+        // Reset tracking data
+        resetTrackingData()
+    }
+
+    private func resetTrackingData() {
+        totalDistance = 0.0
+        maxSpeed = 0.0
+        totalElevationGain = 0.0
+        startTime = nil
+        recordingDuration = 0
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations newLocations: [CLLocation]) {
         guard let newLocation = newLocations.last else { return }
 
-        // Check if the new location is significantly different from the last location
-        if let lastLocation = lastLocation, newLocation.distance(from: lastLocation) < 3 {
-            // If the distance is less than 10 meters, it's not a significant movement, so ignore
-            return
+        // Update current speed and altitude
+        currentSpeed = round(newLocation.speed * 10) / 10 // Round to 1 decimal place
+        currentAltitude = round(newLocation.altitude * 10) / 10
+
+        // Update max speed
+        let roundedSpeed = round(newLocation.speed * 10) / 10
+        if roundedSpeed > maxSpeed {
+            maxSpeed = roundedSpeed
+        }
+            
+            print("Current Speed: \(newLocation.speed)")
+            print("Max Speed: \(maxSpeed)")
+
+        // Add new location to the locations array
+        if let lastLocation = lastLocation {
+            let distance = round(newLocation.distance(from: lastLocation) * 10) / 10
+            totalDistance += distance
+
+            // Calculate elevation gain
+            if newLocation.altitude > lastLocation.altitude {
+                totalElevationGain += round((newLocation.altitude - lastLocation.altitude) * 10) / 10
+            }
         }
 
-        self.lastLocation = newLocation // Update the last location
-        self.locations.append(newLocation) // Add to tracked locations
+        self.lastLocation = newLocation
+        self.locations.append(newLocation)
     }
 }
 
+// Extension methods...
 extension LocationManager {
     func getTrackFiles() -> [String] {
         // List all files in the documents directory
