743a4c2867389b51fb6463c5cd24e1d0a0b34bf4
Rmart7866 <rmarti20@uvm.edu>
2023-11-30 14:39:05 -0500

    Style changes to sign up view

--- SnowCountry/Auth/SignupView.swift
+++ SnowCountry/Auth/SignupView.swift
@@ -2,38 +2,71 @@ import SwiftUI
 
 struct SignupView: View {
     @StateObject var viewModel = SignupViewModel()
-
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Welcome Shredders")
-                .font(.system(size: 40, weight: .bold))
-                .padding(.top, 50)  // Add padding to the top instead of offset
-            
-            TextField("Username", text: $viewModel.username)
-                .textFieldStyle(.plain)
-                .font(.system(size: 25, weight: .bold))
-                .disableAutocorrection(true)
-                .textInputAutocapitalization(.never)
-            
-            TextField("Email", text: $viewModel.email)
-                .textFieldStyle(.plain)
-                .font(.system(size: 25, weight: .bold))
-                .disableAutocorrection(true)
-                .textInputAutocapitalization(.never)
-            
-            SecureField("Password", text: $viewModel.password)
-                .textFieldStyle(.plain)
-                .font(.system(size: 25, weight: .bold))
-            
-            Button("Sign Up") {
-                Task {
-                  try await viewModel.createUser()
+        ZStack {
+            // Background Image
+            Image("background-login")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                Text("Welcome Shredders")
+                    .font(.system(size: 40, weight: .bold))
+                    .padding(.top, 70)
+                    .shadow(color: Color.black, radius: 3, x: 2, y: 2)
+                    .padding(.bottom, 70)
+                
+                TextField("Username", text: $viewModel.username)
+                    .padding()
+                    .textFieldStyle(.plain)
+                    .font(.system(size: 25, weight: .bold))
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                    .textInputAutocapitalization(.never)
+                    .background(Color.black.opacity(0.8))
+                    .cornerRadius(5)
+                    .frame(width: 300, height: 50)
+                    .padding()
+                
+                TextField("Email", text: $viewModel.email)
+                    .padding()
+                    .textFieldStyle(.plain)
+                    .font(.system(size: 25, weight: .bold))
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                    .textInputAutocapitalization(.never)
+                    .background(Color.black.opacity(0.8))
+                    .cornerRadius(5)
+                    .frame(width: 300, height: 50)
+                    .padding()
+                
+                SecureField("Password", text: $viewModel.password)
+                    .padding()
+                    .textFieldStyle(.plain)
+                    .font(.system(size: 25, weight: .bold))
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                    .textInputAutocapitalization(.never)
+                    .background(Color.black.opacity(0.8))
+                    .cornerRadius(5)
+                    .frame(width: 300, height: 50)
+                    .padding()
+                
+                Button("Sign Up") {
+                    Task {
+                        try await viewModel.createUser()
+                    }
                 }
+                .padding()
+                .frame(width: 300, height: 60)
+                .foregroundColor(Color.white)
+                .background(Color.orange)
+                .cornerRadius(5)
+                
+            
             }
-            .frame(width: 100, height: 60)
-            .foregroundColor(Color.white)
-            .background(Color.black)
-            .cornerRadius(15)
+            .padding(.bottom, 50)  // Add padding to the bottom
+            
         }
     }
 }
