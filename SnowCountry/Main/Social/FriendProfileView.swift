ff96df35b7b88f22627cd25f03875063948f37f7
jcastner <jcastner@uvm.edu>
2023-11-30 20:50:24 -0500

    Working add friend & search users. One bug though... the search users view doesn't update the user until you go all the way to the social view.

--- SnowCountry/Main/Social/FriendProfileView.swift
+++ SnowCountry/Main/Social/FriendProfileView.swift
@@ -1,10 +1,25 @@
 import SwiftUI
 
 struct FriendProfileView: View {
-    let user: User
+    @StateObject var viewModel: FriendProfileViewModel
+    
+    init(currentUser: User, focusedUser: User) {
+        self._viewModel = StateObject(wrappedValue: FriendProfileViewModel(currentUser: currentUser, focusedUser: focusedUser))
+    }
     
     var body: some View {
-        Text(user.username)
+        Text(viewModel.focusedUser.username)
+        
+        // If the profile being viewed is not the current users friend...
+        if let currentUserFriends = viewModel.currentUser.friends, currentUserFriends.contains(viewModel.focusedUser.id) {
+            Button("Remove Friend") {
+                Task { try await viewModel.removeFriend() }
+            }
+        } else { // Friend list is not yet initialized
+            Button("Add Friend") {
+                Task { try await viewModel.addFriend() }
+            }
+        }
     }
 }
 
