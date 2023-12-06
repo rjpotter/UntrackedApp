a835f152b5b2beaa597b3d2c3c10f03da3cd2011
Rmart7866 <rmarti20@uvm.edu>
2023-12-05 22:13:11 -0500

    added header logo

--- SnowCountry/Main/SafetyView.swift
+++ SnowCountry/Main/SafetyView.swift
@@ -9,19 +9,26 @@ import SwiftUI
 
 struct SafetyView: View {
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("Avalanche Safety")) {
-                    Text("Avalanche Report")
+        VStack{
+            Text("SnowCountry")
+                .font(Font.custom("Good Times", size:30))
+            
+            NavigationView {
+                List {
+                    Section(header: Text("Avalanche Safety")) {
+                        Text("Avalanche Report")
+                    }
+                    Section(header: Text("Treewell Safety")) {
+                        Text("Treewell Safety Guidelines")
+                    }
+                    Section(header: Text("AR Visualization")) {
+                        Text("AR Top of Run (Coming Soon)")
+                    }
                 }
-                Section(header: Text("Treewell Safety")) {
-                    Text("Treewell Safety Guidelines")
-                }
-                Section(header: Text("AR Visualization")) {
-                    Text("AR Top of Run (Coming Soon)")
-                }
+                .navigationTitle("Safety")
             }
-            .navigationTitle("Safety")
         }
+        .background(Color("Background").opacity(0.5))
     }
+        
 }
