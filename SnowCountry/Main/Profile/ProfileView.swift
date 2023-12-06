1618915d2918dac5192471e8c691a0e808249971
Rmart7866 <rmarti20@uvm.edu>
2023-12-05 22:13:35 -0500

    added header logo

--- SnowCountry/Main/Profile/ProfileView.swift
+++ SnowCountry/Main/Profile/ProfileView.swift
@@ -27,128 +27,145 @@ struct ProfileView: View {
     init(user: User, isMetric: Binding<Bool>) {
         self.user = user
         self._isMetric = isMetric
-       }
+    }
     
     var body: some View {
-        NavigationView {
-            List {
-                ZStack(alignment: .leading) {
-                    // banner image, I want to change it so it can be edited like the profile image
-                    Image("testBannerImage")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(height: 200)
-                        .clipped()
+        VStack{
+            VStack {
+                Text("SnowCountry")
+                    .font(Font.custom("Good Times", size:30))
+                NavigationView {
                     
-                    // profile image, style is stored in components
-                    ProfileImage(user: user, size: ProfileImageSize.large)
-                        .offset(x:70, y: 70)
                     
-                    // username
-                    HStack() {
-                        Text(user.username)
-                            .font(.system(size: 25))
-                            .fontWeight(.semibold)
-                            .offset(x:70, y: 170)
-                    }
-                    
-                    // DARK MODE BUTTON
-                    HStack(){
-                        Button(action: {
-                            isDarkMode.toggle()
-                        }) {
-                            Image(systemName: isDarkMode ? "moon.fill" : "moon")
+                    List {
+                        ZStack(alignment: .leading) {
+                            
+                            // banner image, I want to change it so it can be edited like the profile image
+                            Image("testBannerImage")
                                 .resizable()
-                                .frame(width: 30, height: 30, alignment: .trailing)
-                                .foregroundColor(isDarkMode ? .blue : .blue)
-                                .clipped().buttonStyle(BorderlessButtonStyle())
-                                .fixedSize()
-                                .padding(.leading, 50)
+                                .aspectRatio(contentMode: .fill)
+                                .frame(height: 200)
+                                .clipped()
+                            
+                            // profile image, style is stored in components
+                            ProfileImage(user: user, size: ProfileImageSize.large)
+                                .offset(x:70, y: 70)
+                            
+                            // username
+                            HStack() {
+                                Text(user.username)
+                                    .font(.system(size: 25))
+                                    .fontWeight(.semibold)
+                                    .offset(x:70, y: 170)
+                            }
+                            
+                            // DARK MODE BUTTON
+                            HStack(){
+                                Button(action: {
+                                    isDarkMode.toggle()
+                                }) {
+                                    Image(systemName: isDarkMode ? "moon.fill" : "moon")
+                                        .resizable()
+                                        .frame(width: 30, height: 30, alignment: .trailing)
+                                        .foregroundColor(isDarkMode ? .blue : .blue)
+                                        .clipped().buttonStyle(BorderlessButtonStyle())
+                                        .fixedSize()
+                                        .padding(.leading, 50)
+                                }
+                                .buttonStyle(ClippedButtonStyle())
+                                .offset(x: 185, y: 217)
+                            }
+                            
+                            // EDIT PROFILE BUTTON
+                            VStack(alignment: .leading) {
+                                Button(action: {
+                                    showEditProfile.toggle()
+                                }) {
+                                    HStack {
+                                        Rectangle()
+                                            .fill(Color.gray.opacity(0.5))
+                                            .frame(width: 150, height: 50)
+                                            .cornerRadius(8)
+                                            .shadow(color: Color.black.opacity(0.3), radius: 3, x: 0, y: 2)
+                                            .overlay(
+                                                Text("Edit Profile")
+                                                    .font(.system(size: 15))
+                                                    .fontWeight(.bold)
+                                                    .foregroundColor(Color.black)
+                                            )
+                                    }
+                                    .frame(width: 100, height: 60)
+                                }
+                                .buttonStyle(BorderlessButtonStyle())
+                            }
+                            .offset(x: 80, y: 217)
+                            .padding()
                         }
-                        .buttonStyle(ClippedButtonStyle())
-                        .offset(x: 185, y: 217)
+                        .padding(.top, -12)
+                        .padding(.bottom, 150)
+                        
+                        Section(header: Text("Run History")) {
+                            Button("View Track History") {
+                                showTrackHistoryList = true
+                            }
+                            .padding()
+                            .foregroundColor(.black)
+                            .fontWeight(.bold)
+                            .background(Color.gray.opacity(0.5))
+                            .cornerRadius(10)
+                        }
+                        .sheet(isPresented: $showTrackHistoryList) {
+                            TrackHistoryListView(locationManager: locationManager, isMetric: $isMetric)
+                        }
+                        
+                        Section(header: Text("Settings")) {
+                            Toggle(isOn: $isMetric) {
+                                HStack {
+                                    Text("Units: ")
+                                    Text(isMetric ? "Metric" : "Imperial")
+                                }
+                            }
+                            Button(action: {
+                                showAlert = true
+                            }) {
+                                Text("Logout")
+                                    .accentColor(.red)
+                            }
+                            .alert(isPresented: $showAlert) {
+                                Alert(
+                                    title: Text("Log Out"),
+                                    message: Text("Are you sure you want to log out?"),
+                                    primaryButton: .default(Text("Cancel")),
+                                    secondaryButton: .destructive(Text("Log Out"), action: {
+                                        // Perform logout action here
+                                        AuthService.shared.signOut()
+                                    })
+                                )
+                            }
+                        }
+                        
                     }
                     
-                    // EDIT PROFILE BUTTON
-                    VStack(alignment: .leading) {
-                        Button(action: {
-                            showEditProfile.toggle()
-                        }) {
-                            HStack {
-                                Rectangle()
-                                    .fill(Color.gray.opacity(0.5))
-                                    .frame(width: 150, height: 50)
-                                    .cornerRadius(8)
-                                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 0, y: 2)
-                                    .overlay(
-                                        Text("Edit Profile")
-                                            .font(.system(size: 15))
-                                            .fontWeight(.bold)
-                                            .foregroundColor(Color.black)
-                                    )
-                            }
-                            .frame(width: 100, height: 60)
-                        }
-                        .buttonStyle(BorderlessButtonStyle())
+                    .preferredColorScheme(isDarkMode ? .dark : .light)
+                    
+                    
+                    
+                    
+                    
+                    .fullScreenCover(isPresented: $showEditProfile) {
+                        EditProfileView(user: user)
                     }
-                    .offset(x: 80, y: 217)
-                    .padding()
                 }
-                .padding(.top, -12)
-                .padding(.bottom, 150)
                 
-                Section(header: Text("Run History")) {
-                    Button("View Track History") {
-                        showTrackHistoryList = true
-                    }
-                    .padding()
-                    .foregroundColor(.black)
-                    .fontWeight(.bold)
-                    .background(Color.gray.opacity(0.5))
-                    .cornerRadius(10)
-                }
-                .sheet(isPresented: $showTrackHistoryList) {
-                    TrackHistoryListView(locationManager: locationManager, isMetric: $isMetric)
-                }
-
-                Section(header: Text("Settings")) {
-                    Toggle(isOn: $isMetric) {
-                        HStack {
-                            Text("Units: ")
-                            Text(isMetric ? "Metric" : "Imperial")
-                        }
-                    }
-                    Button(action: {
-                        showAlert = true
-                    }) {
-                        Text("Logout")
-                            .accentColor(.red)
-                    }
-                    .alert(isPresented: $showAlert) {
-                        Alert(
-                            title: Text("Log Out"),
-                            message: Text("Are you sure you want to log out?"),
-                            primaryButton: .default(Text("Cancel")),
-                            secondaryButton: .destructive(Text("Log Out"), action: {
-                                // Perform logout action here
-                                AuthService.shared.signOut()
-                            })
-                        )
-                    }
+                
+                .padding(.leading, -20)
+                .padding(.trailing, -20)
+                .onAppear {
+                    self.isDarkMode = self.colorScheme == .dark
                 }
             }
-            .background(Color(UIColor.systemBackground))
-            .preferredColorScheme(isDarkMode ? .dark : .light)
-            .navigationTitle("User Profile")
-            .fullScreenCover(isPresented: $showEditProfile) {
-                EditProfileView(user: user)
-            }
         }
-        .padding(.leading, -20)
-        .padding(.trailing, -20)
-        .onAppear {
-            self.isDarkMode = self.colorScheme == .dark
-        }
+        .background(Color("Background").opacity(0.5))
     }
 }
 
@@ -170,4 +187,8 @@ extension LocationManager {
             print("Could not delete file: \(error)")
         }
     }
+}
+
+extension Color {
+    static let systemBackground = Color("Background")
 }
