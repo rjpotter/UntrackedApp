ba967ab51f15d8293ebc46e99135bb121fb5ea5a
Rmart7866 <rmarti20@uvm.edu>
2023-12-05 22:10:29 -0500

    Styled the individual posts to make them cleaner. ill probably touch it up a little more soon

--- SnowCountry/Components/PostCell.swift
+++ SnowCountry/Components/PostCell.swift
@@ -3,27 +3,65 @@ import Kingfisher
 
 struct PostCell: View {
     let post: Post
-    
-    var body: some View {
-        VStack {
-            if let user = post.user {
-                HStack {
-                    ProfileImage(user: user, size: ProfileImageSize.xsmall)
+ 
+
+    var body: some View{
+        VStack{
+            VStack(alignment: .leading, spacing: 0) {
+                if let user = post.user {
+                    HStack {
+                        ProfileImage(user: user, size: ProfileImageSize.xsmall)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                            .shadow(radius: 4)
+                            .padding(8)
+                        
+                        
+                        Text(user.username)
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            
+                    }
+                    .padding(.vertical, 4)
                     
-                    Text(user.username)
+                }
+                
+                if let imageURL = post.imageURL  {
+                    KFImage(URL(string: imageURL))
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Rectangle())
+                        .padding(.bottom, 8)
+                }
+                if let user = post.user {
+                    HStack{
+                        // Add the caption below the image
+                        Text(user.username)
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .padding(.horizontal, 2)
+                            .padding(.vertical, 2)
+                            .padding(.bottom, 2)
+                            .padding(.leading, 5)
+                            
+                        Text(post.caption)
+                            .font(.caption)
+                            .padding(.horizontal, 8)
+                            .padding(.vertical, 2)
+                            .padding(.bottom, 2)
+                            .lineLimit(nil)
+                    }
                 }
             }
-            
-            if let imageURL = post.imageURL {
-                KFImage(URL(string: imageURL))
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 400)
-                    .clipShape(Rectangle())
-            }
-            
-            
-            Text(post.caption)
+            .overlay(
+                Rectangle()
+                    .stroke(Color.gray.opacity(0.4), lineWidth: 0.5)
+            )
+            .padding(.horizontal, -2)
+            .background(Color("Base"))
+            .shadow(radius: 5)
         }
+        
     }
+   
 }
