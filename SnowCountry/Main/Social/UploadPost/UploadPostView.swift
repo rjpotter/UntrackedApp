9a1be0c9d33e4a4ba1376b44ae94fa0889f0ab51
Rmart7866 <rmarti20@uvm.edu>
2023-12-07 18:12:13 -0500

    structured upload post page

--- SnowCountry/Main/Social/UploadPost/UploadPostView.swift
+++ SnowCountry/Main/Social/UploadPost/UploadPostView.swift
@@ -5,60 +5,109 @@ struct UploadPostView: View {
     let user: User
     @StateObject var viewModel = UploadPostViewModel()
     @State private var caption = ""
+    
     @State private var imageSelectorShown = false
     @Environment(\.dismiss) var dismiss
-//    @Binding var tabIndex: Int
-
+    //    @Binding var tabIndex: Int
+    
     var body: some View {
-        VStack {
-            // Tool bar
-            HStack {
-                Button {
-                    caption = ""
-                    viewModel.selectedImage = nil
-                    viewModel.postImage = nil
-                    dismiss()
-//                    tabIndex = 0
-                } label: {
-                    Text("Cancel")
-                }
-                
-                Spacer()
-                
-                Text("Create a new post")
-                
-                Spacer()
-                
-                Button {
-                    Task {
-                        try await viewModel.uploadPost(caption: caption)
-                        print(caption)
+        VStack{
+            VStack {
+                Text("SnowCountry")
+                    .font(Font.custom("Good Times", size:30))
+                HStack{
+                    Button {
                         caption = ""
+                        
                         viewModel.selectedImage = nil
                         viewModel.postImage = nil
                         dismiss()
-//                        tabIndex = 0
+                                        
+                    } label: {
+                        Image(systemName: "arrow.left")
+                            .foregroundColor(.cyan)
+                            .imageScale(.large)
+                            .padding(.trailing, 8)
                     }
-                } label: {
-                    Text("Upload")
+                    
+                    Spacer()
+                    Text("Create a new post")
+                        .font(.headline)
+                    Spacer()
+                    Button {
+                        Task {
+                            try await viewModel.uploadPost(caption: caption)
+                            print(caption)
+                            
+                            caption = ""
+                            
+                            viewModel.selectedImage = nil
+                            viewModel.postImage = nil
+                            dismiss()
+                           
+                        }
+                    } label: {
+                        Text("Share")
+                            .foregroundColor(.cyan)
+                            .padding(.trailing, 8)
+                    }
                 }
+                VStack(){
+                    VStack {
+                        if let image = viewModel.postImage {
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                            
+                            
+                            Button {
+                                imageSelectorShown.toggle()
+                            } label: {
+                                Text("Change Image")
+                                    .foregroundColor(.cyan)
+                            }
+                        }
+                        
+                        else{
+                            Button(action: {
+                                imageSelectorShown.toggle()
+                                
+                                
+                            }){
+                                Image(systemName: "plus.circle.fill")
+                                    .resizable()
+                                    .foregroundColor(.cyan)
+                                
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 24, height: 24)
+                                
+                                
+                                Text("Choose Image")
+                                    .foregroundColor(.cyan)
+                                
+                            }
+                        }
+                        
+                    }
+                    .navigationBarBackButtonHidden(true)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .onAppear{ imageSelectorShown.toggle() }
+                    .photosPicker(isPresented: $imageSelectorShown, selection: $viewModel.selectedImage)
+                }
+                .background(Color("Base").opacity(0.5))
             }
-            .padding(.horizontal)
-            
-            HStack {
-                if let image = viewModel.postImage {
-                    image
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 100, height: 100)
-                        .clipped()
-                }
+            .background(Color("Background").opacity(0.5))
+            HStack{
                 
                 TextField("Enter caption...", text: $caption, axis: .vertical)
+                    .keyboardType(.default)
+                   
             }
+            .padding()
+            .background(Color("Background"))
+            .frame( height: 100)
+           
         }
-        .navigationBarBackButtonHidden(true)
-        .onAppear{ imageSelectorShown.toggle() }
-        .photosPicker(isPresented: $imageSelectorShown, selection: $viewModel.selectedImage)
+        .background(Color("Background").opacity(0.5))
     }
 }
